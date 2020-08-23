23116e71fc8db1f74827419cb9d36a731731fdc0
Claudio <claudiorafsilva@gmail.com>
2020-08-23 15:21:29 +0100

    adicionar localizacao e arranjar gramatica

---
+++ src/translations.js
@@ -0,0 +1,29 @@
+const translations = {
+  en: {
+    translation: {
+      youAreWatching: 'You\'re watching',
+      paused: 'Paused',
+      tryAccessingOtherQuality: 'Try changing the image quality',
+      goBack: 'Go Back',
+      speeds: 'Speed',
+      nextEpisode: 'Next Episode',
+      playlist: 'Playlist',
+      playError: 'Something went wrong playing this video',
+    },
+  },
+  pt: {
+    translation: {
+      youAreWatching: 'Você está assistindo',
+      paused: 'Pausado',
+      tryAccessingOtherQuality: 'Tente acessar por outra qualidade',
+      goBack: 'Voltar à navegação',
+      speeds: 'Velocidades',
+      nextEpisode: 'Próximo Episódio',
+      playlist: 'Lista de Reprodução',
+      playError: 'Ocorreu um erro ao tentar reproduzir este vídeo -_-',
+    },
+  },
+
+};
+
+export default translations;
